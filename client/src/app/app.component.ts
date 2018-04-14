2d5ff6d92cb5ffb9474bc9233f274f79e907549e
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2018-04-14 11:15:56 +0300

    add some components list, registration, update login, add

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  authenticated = true;
+  authenticated = false;
   site_name = 'Team-ADIN';
   isAuthenticated() {
     return this.authenticated;
---
+++ client/src/app/data/size.ts
@@ -0,0 +1,4 @@
+export class Size {
+  id: string;
+  title: string;
+}
