478f1c3e67e984137dfb7f36e9b47f226dcb579e
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2018-04-15 10:13:10 +0300

    add functions to http-service

--- client/src/app/service/http-service.service.ts
+++ client/src/app/service/http-service.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {ShopItem} from "../data/shop-item";
+import {Subscribe, SubscribeWithName} from "../data/subscribe";
 
 @Injectable()
 export class HttpService {
@@ -10,6 +11,19 @@ export class HttpService {
 
   public getItems(url: string): Observable<ShopItem[]> {
     return this.http.get<ShopItem[]>(url);
+  }
+
+  public getSubscribes(url: string): Observable<SubscribeWithName[]> {
+    return this.http.get<SubscribeWithName[]>(url);
+  }
+
+  public deleteUnsubscribe(url: string, userInfoId: string, shopItemId: string): Observable<any> {
+    const params = new HttpParams().set('userInfoId', userInfoId).append('shopItemId', shopItemId);
+    return this.http.delete(url, {params});
+  }
+
+  public postSubscribe(url: string, subscribe: Subscribe): Observable<any> {
+    return this.http.post(url, {subscribe});
   }
 
 }
