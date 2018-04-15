c60f8dfcbffe92d7262504fb0c0bda1eb0d0e5ec
Igor Konovalov <rogee.nok@gmail.com>
2018-04-15 13:00:40 +0300

    ui fixes

--- client/src/app/list-component/list.component.ts
+++ client/src/app/list-component/list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {ShopItem} from "../data/shop-item";
 import {HttpService} from "../service/http-service.service";
 import {AppComponent} from "../app.component";
+import {Subscribe} from "../data/subscribe";
 
 @Component({
   selector: 'list',
@@ -18,6 +19,14 @@ export class ListComponent implements OnInit {
 
   ngOnInit() {
     this.http.getItems(AppComponent.globalUrl + this.url + this.urlAll).subscribe(data => this.items = data);
+  }
+
+  submit(itemId: string) {
+    const postSubscribe = new Subscribe();
+    postSubscribe.date = Date.now().toLocaleString();
+    postSubscribe.userInfoId = '101';
+    postSubscribe.shopItemId = itemId;
+    postSubscribe.count = '1';
   }
 
 }
