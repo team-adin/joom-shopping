dd1110c96728359a5d24df625be18c351079954c
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2018-04-15 12:00:00 +0300

    last commit with no consequences

--- client/src/app/show-component/show.component.ts
+++ client/src/app/show-component/show.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {SubscribeWithName} from "../data/subscribe";
+import {HttpService} from "../service/http-service.service";
 
 @Component({
   selector: 'show',
@@ -6,7 +8,12 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
+  subscribes: SubscribeWithName[] = [];
+  url: string = '/api/subscribes/get/101';
   ngOnInit() {
+    this.http.getSubscribes(this.url).subscribe(data => this.subscribes == data);
   }
+  constructor(private http:HttpService) {
 
+  }
 }
